6eb7c62fe525ea1161f942fc62cb1c1bdb6a4794
ahal0309 <ahalabduljaleel8@gmail.com>
2026-07-27 18:07:16 +0530

    Increase logo size in navbar

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -86,13 +86,13 @@ export default function Navigation() {
       >
         {/* Left: New Architectural Logo */}
         <Link href="/home" className="absolute left-4 sm:left-6 md:left-12 top-1/2 -translate-y-1/2 z-10 flex items-center group cursor-pointer pointer-events-auto">
-          <motion.div style={{ scale: logoScale }} className="relative flex items-center justify-center min-w-[44px] min-h-[44px]">
+          <motion.div style={{ scale: logoScale }} className="relative flex items-center justify-center min-w-[64px] min-h-[64px]">
             <Image 
               src="/logo_new.jpeg" 
               alt="PLAN Bë Logo" 
-              width={100} 
-              height={100} 
-              className="w-12 md:w-16 h-auto object-contain transition-transform duration-500 group-hover:scale-105" 
+              width={150} 
+              height={150} 
+              className="w-16 md:w-24 h-auto object-contain transition-transform duration-500 group-hover:scale-105" 
             />
           </motion.div>
         </Link>
